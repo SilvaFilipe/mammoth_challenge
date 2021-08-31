8be5b2fd4c150e4a63e05de5ca58329987c7993f
SilvaFilipe <silva.filipef@gmail.com>
2021-08-31 17:43:25 +0100

    chore: initial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="grey lighten-3" flat>
+      <v-avatar class="mr-10" color="grey darken-1" size="32"></v-avatar>
+
+      <v-btn v-for="link in links" :key="link" text>
+        {{ link }}
+      </v-btn>
+
+      <v-spacer />
+
+      <v-responsive max-width="260">
+        <v-text-field
+          dense
+          flat
+          hide-details
+          rounded
+          solo-inverted
+        ></v-text-field>
+      </v-responsive>
+    </v-app-bar>
+
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="1">
+            <div>
+              Add
+            </div>
+          </v-col>
+
+          <v-col>
+            <v-sheet min-height="70vh" rounded="lg"> </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data: () => ({
+    links: ["Dashboard", "Messages"],
+  }),
+};
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
