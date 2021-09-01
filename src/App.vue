eac9e8b792580e815d22e91fb35338ba670ec2d5
SilvaFilipe <silva.filipef@gmail.com>
2021-09-01 22:54:01 +0100

    chore: add app nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
   <v-app>
     <v-app-bar app color="grey lighten-3" flat>
-      <v-avatar class="mr-10" color="grey darken-1" size="32"></v-avatar>
-
-      <v-btn v-for="link in links" :key="link" text>
-        {{ link }}
-      </v-btn>
+      <v-img max-height="20" max-width="20" src="./assets/mammoth_logo.png" />
+      <div class="ml-3">
+        <span>Data Library > </span>
+        <span class="primary--text">Postgre</span>
+      </div>
 
       <v-spacer />
-
-      <v-responsive max-width="260">
-        <v-text-field
-          dense
-          flat
-          hide-details
-          rounded
-          solo-inverted
-        ></v-text-field>
-      </v-responsive>
+      <v-btn depressed tile color="secondary" style="text-transform: none;">
+        Payment due! Fix now!
+      </v-btn>
+      <v-icon color="primary" class="ml-2">
+        mdi-flash-outline
+      </v-icon>
+      <v-icon color="grey" class="ml-2">
+        mdi-bell-outline
+      </v-icon>
+      <v-icon color="grey" class="ml-4">
+        mdi-help-circle-outline
+      </v-icon>
+      <v-divider vertical inset class="mx-2" />
+      <v-icon color="grey">
+        mdi-account-circle-outline
+      </v-icon>
     </v-app-bar>
 
     <v-main class="grey lighten-3">
@@ -33,9 +39,6 @@ import Files from "@/components/Files";
 
 export default {
   components: { Files },
-  data: () => ({
-    links: ["Dashboard", "Messages"],
-  }),
 };
 </script>
 
