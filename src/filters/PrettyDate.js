c74777958bf186a655a526d33da5197b7d0ab33c
SilvaFilipe <silva.filipef@gmail.com>
2021-09-02 11:22:02 +0100

    fix: pretty date filter

--- src/filters/PrettyDate.js
+++ src/filters/PrettyDate.js
@@ -1,8 +1,11 @@
 
 export default function(value) { 
-    const date = new Date(value)
-    const months = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
-    return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()} ${date.toLocaleTimeString()}`;
+  const date = new Date(value)
+  const months = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"];
+
+  const hours = date.getHours() < 10 ? '0' + date.getHours() :  date.getHours();
+  const minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() :  date.getMinutes();
+
+  return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()} ${hours}:${minutes}`;
 }
